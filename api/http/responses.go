d0230ee7433225fb0ecd7d33bda95fdcaa2bd867
nyagamunene <stevenyaga2014@gmail.com>
2025-09-15 19:03:02 +0300

    fix failing linter
    Signed-off-by: nyagamunene <stevenyaga2014@gmail.com>

--- api/http/responses.go
+++ api/http/responses.go
@@ -78,22 +78,6 @@ func (res deleteCertRes) Empty() bool {
 	return true
 }
 
-type requestCertDownloadTokenRes struct {
-	Token string `json:"token"`
-}
-
-func (res requestCertDownloadTokenRes) Code() int {
-	return http.StatusOK
-}
-
-func (res requestCertDownloadTokenRes) Headers() map[string]string {
-	return map[string]string{}
-}
-
-func (res requestCertDownloadTokenRes) Empty() bool {
-	return false
-}
-
 type issueCertRes struct {
 	SerialNumber string    `json:"serial_number"`
 	Certificate  string    `json:"certificate,omitempty"`
